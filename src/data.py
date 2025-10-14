23746d046bfc1026a8e7d7f987ea8542426e8619
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2025-10-14 12:59:08 +0530

    add dataloader for Cifar100

--- src/data.py
+++ src/data.py
@@ -73,8 +73,9 @@ class Cifar10(Dataset):
 
         self.batch_size = batch_size
         self.num_batches = len(self.labels) // self.batch_size
-        images = self.images.reshape(self.num_batches, self.batch_size, *self.images.shape[1:])
-        labels = torch.from_numpy(self.labels).reshape(self.num_batches, self.batch_size)
+        n = self.num_batches * self.batch_size
+        images = self.images[:n].reshape(self.num_batches, self.batch_size, *self.images.shape[1:])
+        labels = torch.from_numpy(self.labels[:n]).reshape(self.num_batches, self.batch_size)
 
         for i in range(self.num_batches):
             yield images[i], labels[i]
@@ -86,6 +87,51 @@ class Cifar10(Dataset):
         return self.images[idx], self.labels[idx]
 
 
+class Cifar100(Cifar10):
+    path = Path("data/cifar100/CIFAR-100")
+    files_raw = ["meta", "train", "test" ]
+    files = ["images.npy", "labels.npy", "coarse_labels.npy"]
+
+    def __init__(self, test=False, coarse=False):
+        super(Cifar100, self).__init__()
+        self.files_raw = [self.path / f for f in self.files_raw]
+        self.files = [self.path / f for f in self.files]
+
+        if all([f.exists() for f in self.files]):
+            # load pre-processed data
+            self.images = np.load(self.files[0], allow_pickle=True)
+            self.labels = np.load(self.files[2 if coarse else 1], allow_pickle=True)
+        else:
+            # pre-process data and save
+            self._preProcess(coarse)
+
+        self.images = torch.from_numpy(self.images.astype(np.float32) / 255)
+        if test:
+            self.images, self.labels = self.images[-10_000:], self.labels[-10_000:]
+        else:
+            self.images, self.labels = self.images[:-10_000], self.labels[:-10_000]
+
+        self.batch_size = len(self.labels)
+        self.num_batches = 1
+
+    def _preProcess(self, coarse=False):
+        ''' Saves the raw data in numpy format'''
+
+        data = [self._loadFromFile(f) for f in self.files_raw[1:]]
+        self.images = np.r_[*[i for i, _, _ in data]]
+        self.labels = np.r_[*[l for _, l, _ in data]]
+        self.coarse_labels = np.r_[*[l for _, _, l in data]]
+        np.save(self.files[0], self.images)
+        np.save(self.files[1], self.labels)
+        np.save(self.files[2], self.coarse_labels)
+        if coarse: self.labels = self.coarse_labels
+
+    def _loadFromFile(self, filename):
+        ''' Returns (images[_, 3, 32, 32], labels)'''
+        with open(filename, 'rb') as f:
+            data = pickle.load(f, encoding='bytes')
+        return data[b'data'].reshape(-1, 3, 32, 32), data[b'fine_labels'], data[b'coarse_labels']
+    
 
 
 class Cifar10C(Dataset):
@@ -122,8 +168,9 @@ class Cifar10C(Dataset):
 
         self.batch_size = batch_size
         self.num_batches = len(self.labels) // self.batch_size
-        images = self.images.reshape(self.num_batches, self.batch_size, *self.images.shape[1:])
-        labels = torch.from_numpy(self.labels).reshape(self.num_batches, self.batch_size)
+        n = self.num_batches * self.batch_size
+        images = self.images[:n].reshape(self.num_batches, self.batch_size, *self.images.shape[1:])
+        labels = torch.from_numpy(self.labels)[:n].reshape(self.num_batches, self.batch_size)
 
         for i in range(self.num_batches):
             yield images[i], labels[i]
